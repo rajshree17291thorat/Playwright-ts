7dc3c23f5b110c860caf661c2e38e87a0e8ffd6e
Rajshree Thorat <rajshree.thorat@kanini.com>
2026-04-06 16:21:55 +0530

    screenshot changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,8 +34,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    screenshot: 'on',
-    //    'only-on-failure',
+    screenshot:     'only-on-failure',
     video: 'retain-on-failure',
     headless: false,
   },
